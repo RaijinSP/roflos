b18443cdbd2b0bee4ef166c1a058afda44d567ce
RaijinSP <57528925+RaijinSP@users.noreply.github.com>
2020-05-29 18:06:43 +0300

    Removed unused impl
    Removed unnecessary implementation of Serializable interface

--- src/main/com/raijin/serializer/Serializer.java
+++ src/main/com/raijin/serializer/Serializer.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Serializer implements Serializable {
+public class Serializer {
 
     public static Map<String, Object> serialize(final Object obj)
             throws InvocationTargetException, IllegalAccessException {
